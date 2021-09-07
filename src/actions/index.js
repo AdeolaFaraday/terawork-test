ca2ea0a622d47446cfadb04bc6db7e0e6f734f33
AdeolaFaraday <adexconly@gmail.com>
2021-09-07 04:02:17 +0000

    watch button style fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { api } from "../config";
 
 export const searchMovie = async (term) => {
   // Return an action
-  const url = `http://www.omdbapi.com/?i=tt3896198&apikey=${api}&&s=${term}`
+  const url = `https://www.omdbapi.com/?i=tt3896198&apikey=${api}&&s=${term}`
   const request = await axios.get(url);
   const result = request.data.Error ? [] : request.data.Search
   console.log(result);
@@ -15,7 +15,7 @@ export const searchMovie = async (term) => {
 
 export const movieDetail = async (term) => {
   // Return an action
-  const url = `http://www.omdbapi.com/?apikey=${api}&i=${term}`
+  const url = `https://www.omdbapi.com/?apikey=${api}&i=${term}`
   const request = await axios.get(url);
   return {
     type: "SEARCH_MOVIE",
